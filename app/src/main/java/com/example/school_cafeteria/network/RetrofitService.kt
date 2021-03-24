aedaa426a2bd56d4b2421569e239041c06559e1f
KyungMin <2004ckck@naver.com>
2021-03-24 19:53:09 +0900

    [Update] @Query 변경

--- app/src/main/java/com/example/school_cafeteria/network/RetrofitService.kt
+++ app/src/main/java/com/example/school_cafeteria/network/RetrofitService.kt
@@ -1,26 +1,27 @@
 package com.example.school_cafeteria
 
 
-import com.example.school_cafeteria.network.Cafeteria_model
+import com.example.school_cafeteria.Model.Cafeteria_model
 import retrofit2.Call
-
-import retrofit2.http.*
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RetrofitService {
-    @GET("/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=F10&SD_SCHUL_CODE=7380292&KEY=f50a392c9e2647e4a030ca8f95d7c568&MLSV_YMD=&Type=json&MMEAL_SC_CODE=1&MMEAL_SC_CODE=2&MMEAL_SC_CODE=3")
+    @GET("hub/mealServiceDietInfo")
     fun getInfo(
-            //  인증키
-            @Query("KEY") KEY: String,
-            //호출 문서(xml, json)
-            @Query("Type") Type: String,
-            //시도교육청코드
-            @Query("ATPT_OFCDC_SC_CODE") ATPT_OFCDC_SC_CODE: String,
-            //표준학교코드
-            @Query("SD_SCHUL_CODE") SD_SCHUL_CODE: String,
-            //식사코드
-            @Query("MMEAL_SC_CODE") MMEAL_SC_CODE: String,
-            //급식일자
-            @Query( "MLSV_YMD") MLSV_YMD: String,
+        //  인증키
+        @Query("KEY") KEY: String = "f50a392c9e2647e4a030ca8f95d7c568",
+        //호출 문서(xml, json)
+        @Query("Type") Type: String = "json",
+        //시도교육청코드
+        @Query("ATPT_OFCDC_SC_CODE") ATPT_OFCDC_SC_CODE: String = "F10",
+        //표준학교코드
+        @Query("SD_SCHUL_CODE") SD_SCHUL_CODE: String = "7380292",
+        //급식일자
+        @Query("MLSV_YMD") MLSV_YMD: String,
+        //식사코드
+        @Query("MMEAL_SC_CODE") MMEAL_SC_CODE: String,
 
-    ) : Call<Cafeteria_model>
+
+        ): Call<Cafeteria_model>
 }
