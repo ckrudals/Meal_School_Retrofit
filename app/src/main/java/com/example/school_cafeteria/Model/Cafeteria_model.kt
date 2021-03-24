0ba5ad37ee323efd0d706b57fc61e2d07ef6c4bb
KyungMin <2004ckck@naver.com>
2021-03-24 19:49:14 +0900

    [Update] json -> dataClass 로 변환

--- app/src/main/java/com/example/school_cafeteria/Model/Cafeteria_model.kt
+++ app/src/main/java/com/example/school_cafeteria/Model/Cafeteria_model.kt
@@ -1,41 +1,18 @@
-package com.example.school_cafeteria.network
+package com.example.school_cafeteria.Model
 
-
-data class Cafeteria_model(var head: Head, var meal: Meal, var mealService: MealServiceDietInfo, var result: RESULT, var row: Row)
-
-
-data class Head(
-        val RESULT: RESULT,
-        val list_total_count: Int
+data class Cafeteria_model(
+    val meal: MealServiceDietInfo,
+    val head: Head,
+    val result: RESULT,
+    val row: Row
 )
 
-data class Meal(
-        val mealServiceDietInfo: List<MealServiceDietInfo>
-)
 
-data class MealServiceDietInfo(
-        val head: List<Head>,
-        val row: List<Row>
-)
 
-data class RESULT(
-        val CODE: String,
-        val MESSAGE: String
-)
 
-data class Row(
-        val ATPT_OFCDC_SC_CODE: String,
-        val ATPT_OFCDC_SC_NM: String,
-        val CAL_INFO: String,
-        val DDISH_NM: String,
-        val MLSV_FGR: String,
-        val MLSV_FROM_YMD: String,
-        val MLSV_TO_YMD: String,
-        val MLSV_YMD: String,
-        val MMEAL_SC_CODE: String,
-        val MMEAL_SC_NM: String,
-        val NTR_INFO: String,
-        val ORPLC_INFO: String,
-        val SCHUL_NM: String,
-        val SD_SCHUL_CODE: String
-)
+
+
+
+
+
+
