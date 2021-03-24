931eb9b084d2bc49ac40d1cff360f68cb6ed953f
KyungMin <2004ckck@naver.com>
2021-03-24 19:53:28 +0900

    [Add] Okhttp 추가

--- app/src/main/java/com/example/school_cafeteria/network/RetrofitBuilder.kt
+++ app/src/main/java/com/example/school_cafeteria/network/RetrofitBuilder.kt
@@ -1,27 +1,39 @@
 package com.example.school_cafeteria.Ui
 
-import com.google.gson.GsonBuilder
+import com.example.school_cafeteria.RetrofitService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 object RetrofitBuilder {
 
-    private var instance: Retrofit? = null
-    private val gson = GsonBuilder().setLenient().create()
+
     // 서버 주소
-    private const val BASE_URL = "https://open.neis.go.kr/hub/"
+    private const val BASE_URL = "https://open.neis.go.kr/"
 
-    // SingleTon
-    fun getInstance(): Retrofit {
-        if (instance == null) {
-            instance = Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build()
-        }
 
-        return instance!!
+    var service: RetrofitService
+
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(createOkHttpClient())
+            .build()
+
+
+        service = retrofit.create(RetrofitService::class.java)
+    }
+
+    private fun createOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        builder.addInterceptor(interceptor)
+        return builder.build()
     }
 }
 
