7cbe121ee2c3a77c87ed2221c5e5a782318a6ab0
KyungMin <2004ckck@naver.com>
2021-03-25 19:01:04 +0900

    [Update] onResponse 함수 수정

--- app/src/main/java/com/example/school_cafeteria/Ui/MainActivity.kt
+++ app/src/main/java/com/example/school_cafeteria/Ui/MainActivity.kt
@@ -1,94 +1,117 @@
 package com.example.school_cafeteria.Ui
 
+import android.content.ContentValues
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.example.school_cafeteria.Model.meal_model
 import com.example.school_cafeteria.R
-import com.example.school_cafeteria.RetrofitService
+import com.example.school_cafeteria.RetrofitViewModel
 import com.example.school_cafeteria.databinding.MealActivityMainBinding
-import com.example.school_cafeteria.network.Cafeteria_model
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-
-import retrofit2.Retrofit
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
-    lateinit var retrofit: Retrofit
-    lateinit var myAPI: RetrofitService
+
     private val TAG = "MainActivity"
     private val binding by lazy { MealActivityMainBinding.inflate(layoutInflater) }
 
 
+    val current = LocalDateTime.now()
+    val formatter = DateTimeFormatter.ofPattern("YYYYMMdd")
+    val formatted = current.format(formatter)
 
+    private val retrofitViewModel: RetrofitViewModel by viewModels()
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.meal_activity_main)
 
-        val mealPager = binding.mealViewPager
+        binding.dayText.text = formatted
 
-        binding.previousBtn.setOnClickListener {
-            Log.d(TAG, "MainActivity - 이전 버튼 클릭")
+        Retrofit(1)
 
-            mealPager.currentItem = mealPager.currentItem - 1
+        binding.breakBtn.setOnClickListener {
+            Log.d(TAG, "onClick: bereak")
+            Retrofit(1)
         }
 
-        binding.nextBtn.setOnClickListener {
-            Log.d(TAG, "MainActivity - 다음 버튼 클릭")
-            mealPager.currentItem = mealPager.currentItem + 1
+        binding.launchBtn.setOnClickListener {
+            Log.d(TAG, "onClick: lanch")
+            Retrofit(2)
         }
 
+        binding.dinnerBtn.setOnClickListener {
 
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-
-        actionBar?.hide()
-
-        // mealPager 설정
+            Log.d(TAG, "onClick: dinner")
+            Retrofit(3)
 
 
+        }
 
-        val current = LocalDateTime.now()
-        val formatter = DateTimeFormatter.ISO_DATE
-        val formatted = current.format(formatter)
-
-
-
-        retrofit = RetrofitBuilder.getInstance()
-        myAPI = retrofit.create(RetrofitService::class.java)
-
-        myAPI.getInfo("f50a392c9e2647e4a030ca8f95d7c568", "json", "F10", "7380292", "1", formatted)
-            .enqueue(object : Callback<Cafeteria_model> {
-                override fun onFailure(call: Call<Cafeteria_model>, t: Throwable) {
-                    t.printStackTrace()
-                }
-
-                override fun onResponse(
-                    call: Call<Cafeteria_model>,
-                    response: Response<Cafeteria_model>
-                ) {
-                    var name = response.body()?.result
-                    if (!response.isSuccessful) {
-
-                        return
-                    }
-
-                    Log.d(TAG, "onResponse: $name")
-
-
-
-                }
-
-
-            })
 
     }
+
+    fun Retrofit(time: Int) {
+        RetrofitBuilder.service.getInfo(MLSV_YMD = formatted, MMEAL_SC_CODE = time).enqueue(object : Callback<meal_model> {
+            override fun onFailure(call: Call<meal_model>, t: Throwable) {
+                t.printStackTrace()
+                Log.d(ContentValues.TAG, "MainActivity - onFailure()")
+            }
+
+            override fun onResponse(
+                    call: Call<meal_model>,
+                    response: Response<meal_model>
+            ) {
+
+                if (response.isSuccessful) {
+                    val res = response.body()?.mealServiceDietInfo?.get(1)?.row
+
+
+
+                    if (res != null) {
+                        for (i in res.indices) {
+                            val obj = res[i]
+                            val row = obj.DDISH_NM
+                            Log.d(TAG, "onResponse: $row")
+                            when (time) {
+                                1 -> {
+                                    binding.text.text = row
+                                    Log.d(TAG, "onResponse: $binding.text.text")
+                                }
+                                2 -> {
+                                    binding.text.text = row
+                                    Log.d(TAG, "onResponse: $binding.text.text")
+                                }
+                                3 -> {
+                                    binding.text.text = row
+                                    Log.d(TAG, "onResponse: $binding.text.text")
+
+                                }
+                            }
+
+                        }
+                    } else {
+                        Log.d(ContentValues.TAG, "onResponse: ${response.code()}")
+                    }
+
+                }
+            }
+
+
+        })
+    }
 }
+
+
+
 
 
